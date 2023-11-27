685102a1275d89697d2ed0be71d89ec83bce315c
Vinícius Morone <vinimorone@gmail.com>
2023-11-27 13:19:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var name = "Vinícius Morone" ; 
+var age = 21 ; 
+var weight = 80 ; 
+var height = 1.82 ;
+console.log(name, age)
+
+var agem = age * 7 ;
+console.log(agem)
+
+var humildes = ["Bruno", "Fred", "Lipe"] ;
+console.log(humildes)
+
+var caract = {
+    name : name,
+    age : age,
+    weight : weight,
+    height : height
+}; 
+console.log(caract)
+
+var group = [
+    {
+        name : "Bruno",
+        age : 21,
+        weight : 85,
+        height : 1.88,
+        tatoos : true
+
+    },
+    {
+        name : "Fred",
+        age : 20,
+        weight : 76,
+        height : 1.72,
+        tatoos : false
+
+    },
+    {
+        name : "Lipe",
+        age : 22,
+        weight : 78,
+        height : 1.79,
+        tatoos : true
+
+    }
+    
+] ;
+console.log(group)
